1d82a9a96e2cc4e7d8ff027ae939691e05c8bbe5
Paula Acosta <Paulaacosta604@gmail.com>
2022-09-29 15:24:30 -0500

    Challenge juego ahorcado

---
+++ Challenge_Ahorcado/js/palabras.js
@@ -0,0 +1,25 @@
+let input = document.getElementById("agregarPalabra");
+let palabrasLocal;
+const btnAgregar = document.getElementById("btnAgregar");
+
+const palabras = [
+  "COLOR",
+  "CARRO",
+  "CABALLO",
+  "CASCO",
+  "PALOMA",
+  "OJO",
+  "LORO",
+  "ARAÑA",
+  "INSECTO",
+  "OBRA",
+  "MARTILLO",
+  "PLANTA",
+  "FOTO",
+  "MAR",
+  "TORTA",
+  "BOTA",
+  "SOMBRA",
+  "TORRE",
+  "REY",
+];
